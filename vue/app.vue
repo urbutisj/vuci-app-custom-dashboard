7e82bb2cd1db2f57f94417485acb51eb3b1c7cf5
Justinas <urbutisj@gmail.com>
2022-11-04 12:36:42 +0200

    Cards Added

--- vue/app.vue
+++ vue/app.vue
@@ -1,103 +1,84 @@
 <template>
   <div>
     <div class="container">
-      <template v-for="card in cardsData">
-        <CardComponent v-for="data in card" :card="data"/>
-      </template>
+      <div v-for="(card, index) in cardsData" :key="index">
+        <CardComponent :card="card">
+          <template v-if="card.usage" v-slot:cpu>
+            <slot>{{card.usage}}</slot>
+          </template>
+        </CardComponent>
+      </div>
     </div>
   </div>
 </template>
 <script>
 import CardComponent from '../components/VuciDashboard/CardComponent.vue'
+import UsageLine from '../components/VuciDashboard/UsageLine.vue'
 export default {
-components: { CardComponent },
+components: { CardComponent, UsageLine },
 data () {
   return {
-    system: [],
-    interfaces: [],
-    netlog: [],
-    syslog: [],
-    eventsCount: 4,
     uptime: 0,
     ramUsage: 0,
     flashUsage: 0,
     lastCPUTime: null,
     cpuUsage: 0,
-    cardsData: {}
+    cardsData: []
   }
 },
 methods: {
   async creatingFinalObject () {
     // Final Object
-    this.cardsData = {
-      system: [{
-        title: 'System',
-        data:
-        [{ title: 'Router Uptime', value: '%t'.format(this.system.uptime) },
-          { title: 'Local Time', value: this.formatLocalTimeUCI(this.system.localtime) },
-          { title: 'Firmware Version', value: this.system.release.revision },
-          { title: 'CPU load', value: this.cpuUsage },
-          { title: 'RAM', value: this.ramUsage },
-          { title: 'FLASH', value: this.flashUsage }
-        ]
-      }],
-      network: [...this.interfaces],
-      netLog: [{
-        title: 'Network Logs',
-        data: this.netlog
-      }],
-      sysLog: [{
-        title: 'System Logs',
-        data: this.syslog
-      }]
-    }
-    console.log(this.cardsData)
+    this.cardsData.push(await this.getSystemInfo())
+    await this.getNetworkInterfaces()
+    this.cardsData.push(await this.getSystemLog())
+    this.cardsData.push(await this.getNetworkLog())
   },
 
   async getSystemInfo () {
-    // System Details
-    this.system = await this.$system.getInfo()
+    const dataObj = { title: 'System', data: [], usage: [] }
+    const system = await this.$system.getInfo()
+    dataObj.data.push({ title: 'Router uptime', value: '%t'.format(system.uptime) })
+    dataObj.data.push({ title: 'Local Device Time', value: this.formatLocalTimeUCI(system.localtime) })
+    dataObj.data.push({ title: 'Firmware Version', value: system.release.revision })
+    dataObj.usage.push({ title: 'CPU load', value: this.cpuUsage })
+    dataObj.usage.push({ title: 'RAM', value: this.ramUsage })
+    dataObj.usage.push({ title: 'FLASH', value: this.flashUsage })
+    console.log(dataObj)
+    return dataObj
   },
 
   async getNetworkInterfaces () {
     // Network Details
-    await this.$network.load().then(() => this.$network.getInterfaces()).then((res) => {
-      res.map(i => {
-        this.interfaces.unshift({
-          title: i.name,
-          data: [
-            {
-              title: 'TYPE',
-              value: i.status.device ? i.status.device : '---'
-            },
-            {
-              title: 'IP ADDRESS',
-              value: Object.prototype.hasOwnProperty.call(i.status, ['ipv4-address'][0]) ? i.status['ipv4-address'][0].address : '---'
-            }]
-        })
-      })
-    })
-    console.log(this.interfaces)
+    let interfaces = []
+    await this.$network.load()
+    interfaces = this.$network.getInterfaces()
+    for (const netinterface of interfaces) {
+      const dataObj = { title: netinterface.name, data: [] }
+      dataObj.data.push({ title: 'Type', value: netinterface.status.device })
+      const address = !netinterface.status['ipv4-address'] ? '-' : netinterface.status['ipv4-address'][0].address
+      dataObj.data.push({ title: 'IP address', value: address })
+      this.cardsData.push(dataObj)
+    }
   },
   async getNetworkLog () {
     // Network Log
-    await this.$rpc.ubus('log', 'read_db', { table: 'NETWORK' }).then(response => {
-      const sliced = response.log.slice(-this.eventsCount).reverse()
-      const newArr = sliced.map((val) => {
-        return { title: this.formatLocalTimeUCI(val.time), value: val.text }
-      })
-      return this.netlog = newArr
+    const dataObj = { title: 'Network Logs', data: [] }
+    await this.$log.get({ table: 'NETWORK', limit: 5 }).then(r => {
+      for (const record of r.log) {
+        dataObj.data.push({ title: this.formatLocalTimeUCI(record.TIME), value: record.TEXT })
+      }
     })
+    return dataObj
   },
   async getSystemLog () {
-    // System Log
-    await this.$rpc.call('system', 'syslog').then(response => {
-      const sliced = response.log.slice(-this.eventsCount).reverse()
-      const newArr = sliced.map((val) => {
-        return { title: this.formatDate(val.datetime), value: val.msg }
-      })
-      this.syslog = newArr
+    const dataObj = { title: 'System logs', data: [] }
+    await this.$log.get({ limit: 5 }).then(r => {
+      for (const record of r.log) {
+        dataObj.data.push({ title: this.formatLocalTimeUCI(record.TIME), value: record.TEXT })
+      }
     })
+    return dataObj
   },
   async update () {
     this.system = await this.$system.getInfo()
@@ -141,17 +122,7 @@ methods: {
     const min = d.getMinutes()
     const sec = d.getSeconds()
     return `${year}-${month}-${date} %02d:%02d:%02d`.format(hour, min, sec)
-  },
-  formatDate (time) {
-    const d = new Date(time)
-    const year = d.getFullYear().toString().padStart(2, 0)
-    const month = (d.getMonth() + 1).toString().padStart(2, 0)
-    const date = d.getDate().toString().padStart(2, 0)
-    const hour = d.getHours()
-    const min = d.getMinutes()
-    const sec = d.getSeconds()
-    return `${year}-${month}-${date} %02d:%02d:%02d`.format(hour, min, sec)
-  },
+  }
 },
 timers: {
   update: { time: 1000, autostart: true, repeat: true, immediate: true },
@@ -163,11 +134,7 @@ timers: {
   }
 },
 async created () {
-  await this.getSystemInfo()
-  await this.getNetworkInterfaces()
-  await this.getSystemLog()
-  await this.getNetworkLog()
-  this.creatingFinalObject()
+  await this.creatingFinalObject()
 }
 }
 </script>
--- vue/components/CardComponent.vue
+++ vue/components/CardComponent.vue
@@ -3,11 +3,12 @@
     <div class="card-details">
       <div class="card-header">
         <h2>{{card.title}}</h2>
+        <slot name="cpu"></slot>
       </div>
       <div class="card-body">
         <div v-for="(cardData, index) in card.data" :key="index" >
-          <label>{{cardData.title}}</label>
-          <div>{{cardData.value}}</div>
+          <h4>{{cardData.title}}</h4>
+          <label>{{cardData.value}}</label>
         </div>
       </div>
     </div>
@@ -15,7 +16,6 @@
 </template>
 
 <script>
-
 export default {
   props: ['card']
 }
@@ -23,18 +23,34 @@ export default {
 <style>
 .card {
   border-radius: 15px;
+  height: 100%;
 }
 .card:hover{
   border-color: rgba(0, 0, 0, 0.09);
   box-shadow: 0 2px 8px rgb(0 0 0 / 9%);
 }
-.card h2,
-.card h3 {
+.card h2 {
   font-family: 'Oswald', sans-serif;
   display: flex;
   align-items: center;
+  margin-bottom: 0;
+}
+.card h4 {
+  font-weight: bold;
+  color: #222;
 }
 .card h2 {
   text-transform: uppercase;
+}
+.card-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.card-body>div {
+  padding: .5rem 0;
+}
+.card-body>div:not(:last-child){
+  border-bottom: 1px solid #bebebe;
 }
 </style>
